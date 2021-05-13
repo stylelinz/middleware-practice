afa6ee12286e138d0680c847d21205bb06c6ec00
StyleLinz_Man <cwchuang1997@gmail.com>
2021-05-13 23:24:19 +0800

    feat: project init + app.js #20210513001

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use((req, res, next) => {
+  const date = new Date().toLocaleString()
+  console.log(`${date} | ${req.method} from ${req.originalUrl}`)
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆 Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`)
+})
